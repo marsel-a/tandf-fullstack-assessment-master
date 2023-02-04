18acf6b027f019f6f89b53087bd4037277f10005
marsel <marsel@getcraft.com>
2023-02-04 19:28:17 +0800

    Update AppointmentResolver

--- core-api/src/resolvers/appointments.ts
+++ core-api/src/resolvers/appointments.ts
@@ -1,24 +1,20 @@
-import { Appointment } from "@/entities/Appointment";
-import { BookAppointmentInput } from "@/models/appointments/BookAppointmentInput";
-import { NotImplementedException } from "@/models/errors/NotImplementedException";
-import { AppointmentService } from "@/services/AppointmentService";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Appointment } from '@/entities/Appointment';
+import { BookAppointmentInput } from '@/models/appointments/BookAppointmentInput';
+import { AppointmentService } from '@/services/AppointmentService';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class AppointmentResolver {
-  constructor(
-    private readonly appointmentService: AppointmentService,
-  ) {}
-  
+  constructor(private readonly appointmentService: AppointmentService) {}
+
   @Query(() => [Appointment])
   async appointments(): Promise<Appointment[]> {
     return this.appointmentService.getAppointments();
   }
 
   @Mutation(() => Appointment)
-  async bookAppointment(
-    @Arg("bookAppointmentInput") bookAppointmentInput: BookAppointmentInput,
-  ): Promise<Appointment> {
-    throw new NotImplementedException("bookAppointment");
+  async bookAppointment(@Arg('bookAppointmentInput') bookAppointmentInput: BookAppointmentInput): Promise<Appointment> {
+    return this.appointmentService.bookAppointment(bookAppointmentInput);
   }
 }
+
