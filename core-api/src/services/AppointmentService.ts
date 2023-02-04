6490fb0ae6a08cc940c57cf93f9067137cccc22d
marsel <marsel@getcraft.com>
2023-02-04 19:28:05 +0800

    Update AppointmentService

--- core-api/src/services/AppointmentService.ts
+++ core-api/src/services/AppointmentService.ts
@@ -1,23 +1,47 @@
-import { Appointment } from "@/entities/Appointment";
-import { BookAppointmentInput } from "@/models/appointments/BookAppointmentInput";
-import { NotImplementedException } from "@/models/errors/NotImplementedException";
-import { Service } from "typedi";
-import { Repository } from "typeorm";
-import { InjectRepository } from "typeorm-typedi-extensions";
+import { Appointment } from '@/entities/Appointment';
+import { Doctor } from '@/entities/Doctor';
+import { BookAppointmentInput } from '@/models/appointments/BookAppointmentInput';
+import { differenceInMinutes } from 'date-fns';
+import { Service } from 'typedi';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
 
 @Service()
 export class AppointmentService {
   constructor(
     @InjectRepository(Appointment)
     private readonly appointmentRepo: Repository<Appointment>,
-   
+    @InjectRepository(Doctor)
+    private readonly doctorRepo: Repository<Doctor>
   ) {}
 
   getAppointments(): Promise<Appointment[]> {
     return this.appointmentRepo.find();
   }
 
-  bookAppointment(options: BookAppointmentInput): Promise<Appointment> {
-    throw new NotImplementedException('bookAppointment not implemented');
+  async bookAppointment(options: BookAppointmentInput): Promise<Appointment> {
+    const doctorId = options.slot.doctorId;
+    const startTime = options.slot.start;
+    const durationMinutes = differenceInMinutes(options.slot.end, options.slot.start);
+
+    const appointmentFound = await this.appointmentRepo.findOne({
+      where: {
+        startTime,
+      },
+    });
+
+    if (appointmentFound) {
+      throw new Error('Appointment slot already taken');
+    }
+
+    const doctor = await this.doctorRepo.findOne({ where: { id: doctorId } });
+    const appointment = new Appointment();
+    appointment.startTime = startTime;
+    appointment.doctor = doctor;
+    appointment.durationMinutes = durationMinutes;
+
+    const newAppointment = this.appointmentRepo.create(appointment);
+    return this.appointmentRepo.save(newAppointment);
   }
 }
+
