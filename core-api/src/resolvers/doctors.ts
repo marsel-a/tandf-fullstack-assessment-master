1313d7e8fb5a744dea9bdf13af10cc26e9a81e88
marsel <marsel@getcraft.com>
2023-02-04 19:26:16 +0800

    Update DoctorResolver

--- core-api/src/resolvers/doctors.ts
+++ core-api/src/resolvers/doctors.ts
@@ -1,33 +1,27 @@
-import { Doctor } from "@/entities/Doctor";
-import { Slot } from "@/models/appointments/Slot";
-import { AddDoctorInput } from "@/models/doctor/AddDoctorInput";
-import { NotImplementedException } from "@/models/errors/NotImplementedException";
-import { DoctorService } from "@/services/DoctorService";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Doctor } from '@/entities/Doctor';
+import { Slot } from '@/models/appointments/Slot';
+import { AddDoctorInput } from '@/models/doctor/AddDoctorInput';
+import { NotImplementedException } from '@/models/errors/NotImplementedException';
+import { DoctorService } from '@/services/DoctorService';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver(() => Doctor)
 export class DoctorResolver {
-  constructor(
-    private readonly doctorService: DoctorService,
-  ) {}
-  
+  constructor(private readonly doctorService: DoctorService) {}
+
   @Query(() => [Doctor])
   async doctors(): Promise<Doctor[]> {
     return this.doctorService.getDoctors();
   }
 
   @Mutation(() => Doctor)
-  async addDoctor(
-    @Arg('doctor') doctor: AddDoctorInput,
-  ): Promise<Doctor> {
-    throw new NotImplementedException("addDoctor: not implemented")
+  async addDoctor(@Arg('doctor') doctor: AddDoctorInput): Promise<Doctor> {
+    throw new NotImplementedException('addDoctor: not implemented');
   }
 
   @Query(() => [Slot])
-  async slots(
-    @Arg('from') from: Date,
-    @Arg('to') to: Date,
-  ): Promise<Slot[]> {
-    throw new NotImplementedException("slots")
+  async slots(@Arg('from') from: Date, @Arg('to') to: Date): Promise<Slot[]> {
+    return this.doctorService.getAvailableSlots(from, to);
   }
 }
+
